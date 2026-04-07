da90725b57c568938614ae670f50a3c4185b86e8
Sahem-test <testroj76@gmail.com>
2026-04-07 20:52:47 +0300

    ready TextBoxTests of pageObject style

--- src/test/java/guru/qa/classWork/TextBoxTests.java
+++ src/test/java/guru/qa/classWork/TextBoxTests.java
@@ -1,49 +1,47 @@
 package guru.qa.classWork;
 
 import guru.qa.TestBase;
+import guru.qa.pages.TextBoxPage;
 import org.junit.jupiter.api.Test;
-
-import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
 import static guru.qa.testData.TestData.*;
 
 public class TextBoxTests extends TestBase {
 
+    TextBoxPage textBoxPage = new TextBoxPage();
 
 
     @Test
     void successfulFillFormTest() {
-        open("/text-box");
-        $("#userName").setValue(userName);
-        $("#userEmail").setValue(userEmail);
-        $("#currentAddress").setValue(currentAddress);
-        $("#permanentAddress").setValue(permanentAddress);
-        $("#submit").click();
+        textBoxPage.openPage();
+        textBoxPage.typeUserName(userName);
+        textBoxPage.typeUserEmail(userEmail);
+        textBoxPage.typeCurrentAddress(currentAddress);
+        textBoxPage.typePermanentAddress(permanentAddress);
+        textBoxPage.submitForm();
 
-        $("#output #name").shouldHave(text(userName));
-        $("#output #email").shouldHave(text(userEmail));
-        $("#output #currentAddress").shouldHave(text(currentAddress));
-        $("#output #permanentAddress").shouldHave(text(permanentAddress));
+        textBoxPage.checkField("name", userName);
+        textBoxPage.checkField("email", userEmail);
+        textBoxPage.checkField("currentAddress", currentAddress);
+        textBoxPage.checkField("permanentAddress", permanentAddress);
     }
 
     @Test
     void successfulFillFormWithoutAddressTest() {
-        open("/text-box");
-        $("#userName").setValue(userName);
-        $("#userEmail").setValue(userEmail);
-        $("#submit").click();
+        textBoxPage.openPage();
+        textBoxPage.typeUserName(userName);
+        textBoxPage.typeUserEmail(userEmail);
+        textBoxPage.submitForm();
 
-        $("#output #name").shouldHave(text(userName));
-        $("#output #email").shouldHave(text(userEmail));
+        textBoxPage.checkField("name", userName);
+        textBoxPage.checkField("email", userEmail);
     }
 
     @Test
     void submitWithMinimalDataTest() {
-        open("/text-box");
-        $("#userName").setValue(userName);
-        $("#submit").click();
-        $("#output #name").shouldHave(text(userName));
+        textBoxPage.openPage();
+        textBoxPage.typeUserName(userName);
+        textBoxPage.submitForm();
+        textBoxPage.checkField("name", userName);
     }
 
 }
